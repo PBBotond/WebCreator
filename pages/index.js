df98951cabae6f8cd2536a7cbe997f85c6c9fa3b
Portik-Bakai Botond <bbotond11@gmail.com>
2021-09-19 17:16:33 +0200

    Base

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Web_Creator</title>
+      </Head>
+      <h1>Hello</h1>
+    </div>
+  );
+}
---
+++ pages/myPage.js
@@ -0,0 +1,9 @@
+const myPage = () => {
+    return (
+        <div>
+            <h1>Hello2.0</h1>
+        </div>
+    )
+}
+
+export default myPage
